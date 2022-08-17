c57e5465fd88a3d5bd3eeb83e199c5127f587aee
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-17 21:28:01 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+class Subtraction{
+public static void main(String ...args){
+System.out.println("The two numbers are:");
+int a=25;
+int b=8;
+System.out.println(a+" "+b);
+int ans=a-b;
+System.out.println("The ans is:"+ans);
+}
+}

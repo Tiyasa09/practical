577f3e54f244605d8d5bf4757266da82890ccbbe
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-17 21:18:21 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+class Multiplication{
+public static void main(String ...args){
+System.out.println("The two numbers are:");
+int a=10;
+int b=5;
+System.out.println(a+" "+b);
+int ans=a*b;
+System.out.println("The ans is:"+ans);
+}
+}

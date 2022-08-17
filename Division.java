27bbd5eaf79a4ab247166eb32e3550ba3a87a61d
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-17 21:49:40 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,10 @@
+class Division{
+public static void main(String []args){
+System.out.println("The two numbers are:");
+int a=25;
+int b=5;
+System.out.println(a+" "+b);
+int result=a/b;
+System.out.println("The result is:"+result);
+}
+}

9b3218e65a1792a458317968f4874a4936ad8aee
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-18 13:12:58 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,27 @@
+package operator1;
+
+public class operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println(a>b&&b>a);
+		System.out.println(a>b||b>a);
+		
+		System.out.println(a++ + ++a);
+		System.out.println(a-- - --a);
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+	}
+
+}

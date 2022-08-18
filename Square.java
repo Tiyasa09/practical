d690dd1690b4cc17f0d477ddf5d71dc62aa5b7ca
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-18 14:12:11 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square{
+public static void main(String ...args){
+System.out.println("The side is:");
+int a=14;
+System.out.println(a);
+int area=a*a;
+System.out.println("The area is:"+area);
+int perimeter=4*a;
+System.out.println("The perimeter is:"+perimeter);
+}
+}

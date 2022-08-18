eb467a097d123ec8540f2e1c3c41c08e8fea59e2
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-18 14:05:30 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle{
+public static void main(String ...args){
+System.out.println("The length and breadth are:");
+int a=15;
+int b=8;
+System.out.println(a+" "+b);
+int perimeter=2*(a+b);
+System.out.println("The perimeter is:"+perimeter);
+int area=a*b;
+System.out.println("The area is:"+area);
+}
+}

c535443f089cfcb50e75c3944495aee00cee07f7
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-20 23:27:12 +0530

    Add files via upload

---
+++ Elseif.java
@@ -0,0 +1,18 @@
+package operator1;
+
+public class Elseif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8;
+		int b=9;
+		if (a+b<10)
+		System.out.println("a+b is less than 10");
+		else {
+	    System.out.println("a+b is greater than 10");
+			
+		}
+
+	}
+
+}

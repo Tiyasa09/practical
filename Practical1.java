8561384b3683e64a4a39e6929ba0ff250841b8eb
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-24 11:44:05 +0530

    Add files via upload

---
+++ Practical1.java
@@ -0,0 +1,16 @@
+package operator1;
+
+public class Practical1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=9;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(i);
+		}
+		System.out.println(" ");
+		}
+
+	}
+
+}

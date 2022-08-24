44f624b54cc52547a4d4238315f5a7a08e4a4040
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-24 11:29:50 +0530

    Add files via upload

---
+++ Foreachloop.java
@@ -0,0 +1,12 @@
+package operator1;
+
+public class Foreachloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {1,2,3,4,5};
+		for(int i:arr) {
+			System.out.println(i);
+		}}}
+
+

cdc2ff4dbf8ffa074e640572a67f6c7c28a74722
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-24 11:41:32 +0530

    Add files via upload

---
+++ Infinityloop.java
@@ -0,0 +1,13 @@
+package operator1;
+
+public class Infinityloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(;;) {
+			System.out.println("Need More Practise");
+		}
+
+	}
+
+}

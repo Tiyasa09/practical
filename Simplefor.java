57a4d4e8d46bc2dceead20e8ca822b383d4ec4ec
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-24 11:08:26 +0530

    Add files via upload

---
+++ Simplefor.java
@@ -0,0 +1,14 @@
+package operator1;
+
+public class Simplefor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			  for(int j=1;j<=i;j++) {
+				  System.out.print("*");
+         }
+		System.out.println();
+		}}}
+
+
